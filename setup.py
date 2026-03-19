214c11999cc7e66bc9af41e6ec916099bd367489
ganu0811 <ganufindsu@gmail.com>
2026-03-19 07:27:07 +0000

    data and notebook experiment added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "WasteDetection",
+    version = '0.0.0',
+    author = 'Ganesh Adarkar',
+    author_email = 'ganesh.adarkar0811@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
+
+
